09162f9a2c4ec41042605eed8cb2e52d49c3c9cf
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-10 14:51:08 +0200

    Pagination 1

--- store/views.py
+++ store/views.py
@@ -1,10 +1,10 @@
+from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django.db.models import Count
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .filters import ProductFilter
